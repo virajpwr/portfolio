f5800bdb4bba41b55c7868110a219cfad0299a12
virajpwr <viraj.telematicus@gmail.com>
2020-10-30 22:59:28 +0530

    changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function openEmail() {
   document.getElementById("mailId").classList.add("showEmail");
-  document.getElementById("mailIdText").innerHTML = "kamaltejag@gmail.com";
+  document.getElementById("mailIdText").innerHTML = "virajp.mail@gmail.com";
 }
